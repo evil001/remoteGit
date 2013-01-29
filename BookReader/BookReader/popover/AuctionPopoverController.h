036166af95c2281c1376faeaa44453231ef24284
wangyuxin <meizi8475@gmail.com>
2013-01-29 18:01:23 +0800

    提交详细页的xib文件

---
+++ BookReader/BookReader/popover/AuctionPopoverController.h
@@ -0,0 +1,14 @@
+//
+//  AuctionPopoverController.h
+//  BookReader
+//
+//  Created by wangyuxin on 13-1-29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuctionPopoverController : UIViewController
+
+
+@end
